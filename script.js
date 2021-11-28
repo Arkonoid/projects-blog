2505305e590aa6dc23492cdb44e8de44e456fdd1
Tyler <tylermc404@gmail.com>
2021-11-28 16:22:49 -0500

    Make progress on game and add RNG function
    Improved the game to the point of letting the players choose a class.
    Also made a function that can be used as a random number generator
    for in game events.
    
    The code new differs between desktop and mobile devices.

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+//Random Number Generator
+function random(min,max) {
+    let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    return randomNumber;
+}
+
 //Classes
 
-warrior = {name: 'Warrior', hp: 100, att: 15, arm: 12, spd: 20, magic: 3};
-mage = {name: 'Mage', hp: 50, att: 5, arm: 5, spd: 35, magic: 25};
-rogue = {name: 'Rogue', hp: 30, att: 10, arm: 7, spd: 45, magic: 10}
+let warrior = {name: 'Warrior', hp: 100, att: 15, arm: 12, spd: 20, magic: 3};
+let mage = {name: 'Mage', hp: 50, att: 5, arm: 5, spd: 35, magic: 25};
+let rogue = {name: 'Rogue', hp: 30, att: 10, arm: 7, spd: 45, magic: 10}
 
 let class_choice;
+
+//Enemies
+let enemy1 = {name: 'Bandit', hp: 35, att: 10, arm: 5, spd: 5, magic: 1};
+let enemy2 = {name: 'Monkey', hp: 25, att: 20, arm: 0, spd: 50, magic: 0};
+let enemy3 = {name: 'Bat', hp: 15, att: 10, arm: 0, spd: 75, magic: 0};
+let enemy4 = {name: 'Cave Troll', hp: 150, att: 40, arm: 5, spd: 0, magic: 3};
+let enemy5 = {name: 'Seagull', hp: 15, att: 10, arm: 0, spd: 65, magic: 0};
+let enemy6 = {name: 'Giant Crab', hp: 75, att: 10, arm: 15, spd: 0, magic: 0};
+
+//Game Logic
 
 document.getElementById('game-start').addEventListener('click', ()=>{
     document.getElementById('game-start').style.display = 'none';
@@ -16,6 +32,56 @@ document.getElementById('game-start').addEventListener('click', ()=>{
         "<button type='button' id='choice-mage'>Mage</button>" +
         "<button type='button' id='choice-rogue'>Rogue</button>";
 
+    if($(window).width() < 700) {
+        $('#choice-warrior').click( ()=> {
+            class_choice = warrior;
+
+            document.getElementById('stats').style.display = 'block';
+            document.getElementById('stats').innerHTML =
+                "HP: " + class_choice.hp + "<br>" +
+                "Attack: " + class_choice.att + "<br>" +
+                "Armor: " + class_choice.arm + "<br>" +
+                "Speed: " + class_choice.spd + "<br>" +
+                "Magic: " + class_choice.magic;
+            
+            document.getElementById('mobile-button').style.display = 'block';
+        });
+
+        $('#choice-mage').click( ()=> {
+            class_choice = mage;
+
+            document.getElementById('stats').style.display = 'block';
+            document.getElementById('stats').innerHTML =
+                "HP: " + class_choice.hp + "<br>" +
+                "Attack: " + class_choice.att + "<br>" +
+                "Armor: " + class_choice.arm + "<br>" +
+                "Speed: " + class_choice.spd + "<br>" +
+                "Magic: " + class_choice.magic;
+            
+            document.getElementById('mobile-button').style.display = 'block';
+        });
+
+        $('#choice-rogue').click( ()=> {
+            class_choice = rogue;
+
+            document.getElementById('stats').style.display = 'block';
+            document.getElementById('stats').innerHTML =
+                "HP: " + class_choice.hp + "<br>" +
+                "Attack: " + class_choice.att + "<br>" +
+                "Armor: " + class_choice.arm + "<br>" +
+                "Speed: " + class_choice.spd + "<br>" +
+                "Magic: " + class_choice.magic;
+            
+            document.getElementById('mobile-button').style.display = 'block'; 
+        });
+
+        $('#mobile-button').click( ()=> {
+            document.getElementById('choose-class').style.display = 'none';
+            document.getElementById('adventure').style.display = 'block';
+        });
+
+    }
+    else {
     $('#choice-warrior').mouseover( ()=>{
         class_choice = warrior;
 
@@ -39,7 +105,7 @@ document.getElementById('game-start').addEventListener('click', ()=>{
         "Speed: " + class_choice.spd + "<br>" +
         "Magic: " + class_choice.magic;
     });
-        
+
     $('#choice-rogue').mouseover( ()=>{
         class_choice = rogue;
 
@@ -50,10 +116,15 @@ document.getElementById('game-start').addEventListener('click', ()=>{
         "Armor: " + class_choice.arm + "<br>" +
         "Speed: " + class_choice.spd + "<br>" +
         "Magic: " + class_choice.magic;
-
-        if($(window).width() < 700) {
-            document.getElementById('mobile-button').style.display = 'block';
-        }
     });
-        
+    }
+});
+
+//Adventure
+
+
+//Battle
+
+$('#random-button').click ( ()=>{
+    document.getElementById('random-number').innerHTML = random(0,9);
 });
